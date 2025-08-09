ab5541fd2dbeed9dc5629935d0711e7b7df09f67
M-Baghestani <mh.baghestani@khu.ac.ir>
2025-08-09 16:57:00 +0330

    make the server.js looks prettier

--- controller/bookController.js
+++ controller/bookController.js
@@ -2,139 +2,140 @@ const BookModel = require("../models/Book.js");
 const wrr = require("./../funcs/writeFunc.js");
 const url = require("url");
 const getAll = async (req, res) => {
-  try {
-    const books = await BookModel.getAll();
+	try {
+		const books = await BookModel.getAll();
 
-    wrr(
-      res,
-      200,
-      { "Content-Type": "application/json" },
-      JSON.stringify(books)
-    );
-  } catch (err) {
-    wrr(
-      res,
-      500,
-      { "Content-Type": "application/json" },
-      { error: err.message }
-    );
-  }
+		wrr(
+			res,
+			200,
+			{ "Content-Type": "application/json" },
+			JSON.stringify(books)
+		);
+	} catch (err) {
+		wrr(
+			res,
+			500,
+			{ "Content-Type": "application/json" },
+			{ error: err.message }
+		);
+	}
 };
 
-const getOne = async (req, res, id) => {
-  try {
-    const bookInfo = await BookModel.getOne(id);
-    wrr(
-      res,
-      201,
-      { "Content-Type": "application/json" },
-      JSON.stringify(bookInfo)
-    );
-  } catch (error) {
-    wrr(
-      res,
-      500,
-      { "Content-Type": "application/json" },
-      { error: error.message }
-    );
-  }
+const getOne = async (req, res) => {
+	try {
+		const id = url.parse(req.url, true).query.id;
+		const bookInfo = await BookModel.getOne(id);
+		wrr(
+			res,
+			201,
+			{ "Content-Type": "application/json" },
+			JSON.stringify(bookInfo)
+		);
+	} catch (error) {
+		wrr(
+			res,
+			500,
+			{ "Content-Type": "application/json" },
+			{ error: error.message }
+		);
+	}
 };
 
 const createBook = async (req, res) => {
-  const bookDB = await BookModel.getAll();
-  let body = "";
-  req.on("data", (chunk) => {
-    body += chunk;
-  });
-  req.on("end", async () => {
-    body = JSON.parse(body);
-    const isBookExist = await bookDB.some(
-      (book) => book.title == body.title && book.author == body.author
-    );
-    if (isBookExist) {
-      wrr(
-        res,
-        409,
-        { "content-type": "application/json" },
-        JSON.stringify({ message: "The book is already exist!" })
-      );
-    } else {
-      const newBook = {
-        id: crypto.randomUUID(),
-        title: body.title,
-        author: body.author,
-        price: body.price,
-        free: 1,
-      };
-      const messageWrite = await BookModel.write(newBook);
-      wrr(
-        res,
-        201,
-        { "content-type": "application/json" },
-        JSON.stringify(messageWrite)
-      );
-    }
-  });
+	const bookDB = await BookModel.getAll();
+	let body = "";
+	req.on("data", (chunk) => {
+		body += chunk;
+	});
+	req.on("end", async () => {
+		body = JSON.parse(body);
+		const isBookExist = await bookDB.some(
+			(book) => book.title == body.title && book.author == body.author
+		);
+		if (isBookExist) {
+			wrr(
+				res,
+				409,
+				{ "content-type": "application/json" },
+				JSON.stringify({ message: "The book is already exist!" })
+			);
+		} else {
+			const newBook = {
+				id: crypto.randomUUID(),
+				title: body.title,
+				author: body.author,
+				price: body.price,
+				free: 1,
+			};
+			const messageWrite = await BookModel.write(newBook);
+			wrr(
+				res,
+				201,
+				{ "content-type": "application/json" },
+				JSON.stringify(messageWrite)
+			);
+		}
+	});
 };
 
 const update = async (req, res) => {
-  const bookDB = await BookModel.getAll();
-  const bookID = url.parse(req.url, true).query.id;
-  let body = "";
-  req.on("data", (chunk) => {
-    body += chunk;
-  });
-  req.on("end", async () => {
-    body = JSON.parse(body);
-    const isBookExist = bookDB.some((book) => book.id == bookID);
-    if (isBookExist) {
-      const theBook = bookDB.find((book) => book.id == bookID);
-      theBook.title = body.title || theBook.title;
-      theBook.author = body.author || theBook.author;
-      theBook.price = body.price || theBook.price;
-      theBook.free = body.free || theBook.free;
+	const bookDB = await BookModel.getAll();
+	const bookID = url.parse(req.url, true).query.id;
+	let body = "";
+	req.on("data", (chunk) => {
+		body += chunk;
+	});
+	req.on("end", async () => {
+		body = JSON.parse(body);
+		const isBookExist = bookDB.some((book) => book.id == bookID);
+		if (isBookExist) {
+			const theBook = bookDB.find((book) => book.id == bookID);
+			theBook.title = body.title || theBook.title;
+			theBook.author = body.author || theBook.author;
+			theBook.price = body.price || theBook.price;
+			theBook.free = body.free || theBook.free;
 
-      const messageUpdate = await BookModel.update(theBook, bookID);
-      wrr(
-        res,
-        201,
-        { "content-type": "application/json" },
-        JSON.stringify(messageUpdate)
-      );
-    } else {
-      wrr(
-        res,
-        201,
-        { "content-type": "application/json" },
-        JSON.stringify({ message: "The Book is not exist!" })
-      );
-    }
-  });
+			const messageUpdate = await BookModel.update(theBook, bookID);
+			wrr(
+				res,
+				201,
+				{ "content-type": "application/json" },
+				JSON.stringify(messageUpdate)
+			);
+		} else {
+			wrr(
+				res,
+				201,
+				{ "content-type": "application/json" },
+				JSON.stringify({ message: "The Book is not exist!" })
+			);
+		}
+	});
 };
 
 const removeOne = async (req, res) => {
-  const bookID = url.parse(req.url, true).query.id;
-  const bookDb = await BookModel.getAll();
+	const bookID = url.parse(req.url, true).query.id;
+	const bookDb = await BookModel.getAll();
 
-  const isBookExist = bookDb.filter((book) => book.id == bookID);
+	const isBookExist = bookDb.filter((book) => book.id == bookID);
 
-  if (isBookExist) {
-    const msg = await BookModel.removeOne(bookID);
-    wrr(res, 201, { "content-type": "application/json" }, JSON.stringify(msg));
-  } else {
-    wrr(
-      res,
-      404,
-      { "content-type": "application/json" },
-      JSON.stringify({ message: "the book is not exist!" })
-    );
-  }
+	if (isBookExist) {
+		const msg = await BookModel.removeOne(bookID);
+		wrr(res, 201, { "content-type": "application/json" }, JSON.stringify(msg));
+	} else {
+		wrr(
+			res,
+			404,
+			{ "content-type": "application/json" },
+			JSON.stringify({ message: "the book is not exist!" })
+		);
+	}
 };
 
 module.exports = {
-  getAll,
-  getOne,
-  createBook,
-  update,
-  removeOne,
+	getAll,
+	getOne,
+	createBook,
+	update,
+	removeOne,
 };
--- server.js
+++ server.js
@@ -1,44 +1,36 @@
 const http = require("http");
-const fs = require("fs");
-const url = require("url");
-const db = require("./db.json");
-const wrr = require("./funcs/writeFunc.js");
 const bookController = require("./controller/bookController.js");
 const userController = require("./controller/userController.js");
 const reserveController = require("./controller/reserveController.js");
 const managerController = require("./controller/managerController.js");
-const { readDB, writeDB } = require("./funcs/dbhelper.js");
-
 const server = http.createServer((req, res) => {
-  if (req.method === "GET" && req.url === "/api/users") {
-    userController.getAll(req, res);
-  } else if (req.method === "POST" && req.url === "/api/books/reserve") {
-    reserveController.reserve(req, res);
-  } else if (req.method === "GET" && req.url.startsWith("/api/users")) {
-    const userID = url.parse(req.url, true).query.id;
-    userController.getOne(req, res, userID);
-  } else if (req.method === "GET" && req.url === "/api/books") {
-    bookController.getAll(req, res);
-  } else if (req.method === "GET" && req.url.startsWith("/api/books")) {
-    const bookID = url.parse(req.url, true).query.id;
-    bookController.getOne(req, res, bookID);
-  } else if (req.method === "POST" && req.url === "/api/users") {
-    userController.createUser(req, res);
-  } else if (req.method === "POST" && req.url === "/api/books") {
-    bookController.createBook(req, res);
-  } else if (req.method === "PUT" && req.url.startsWith("/api/books")) {
-    bookController.update(req, res);
-  } else if (req.method === "PUT" && req.url.startsWith("/api/users")) {
-    userController.updateUser(req, res);
-  } else if (req.method === "DELETE" && req.url.startsWith("/api/users")) {
-    userController.removeOne(req, res);
-  } else if (req.method === "DELETE" && req.url.startsWith("/api/books")) {
-    bookController.removeOne(req, res);
-  } else if (req.method === "POST" && req.url.startsWith("/api/makeManager")) {
-    managerController.addManager(req, res);
-  }
+	if (req.method === "GET" && req.url === "/api/users") {
+		userController.getAll(req, res);
+	} else if (req.method === "POST" && req.url === "/api/books/reserve") {
+		reserveController.reserve(req, res);
+	} else if (req.method === "GET" && req.url.startsWith("/api/users")) {
+		userController.getOne(req, res, userID);
+	} else if (req.method === "GET" && req.url === "/api/books") {
+		bookController.getAll(req, res);
+	} else if (req.method === "GET" && req.url.startsWith("/api/books")) {
+		bookController.getOne(req, res);
+	} else if (req.method === "POST" && req.url === "/api/users") {
+		userController.createUser(req, res);
+	} else if (req.method === "POST" && req.url === "/api/books") {
+		bookController.createBook(req, res);
+	} else if (req.method === "PUT" && req.url.startsWith("/api/books")) {
+		bookController.update(req, res);
+	} else if (req.method === "PUT" && req.url.startsWith("/api/users")) {
+		userController.updateUser(req, res);
+	} else if (req.method === "DELETE" && req.url.startsWith("/api/users")) {
+		userController.removeOne(req, res);
+	} else if (req.method === "DELETE" && req.url.startsWith("/api/books")) {
+		bookController.removeOne(req, res);
+	} else if (req.method === "POST" && req.url.startsWith("/api/makeManager")) {
+		managerController.addManager(req, res);
+	}
 });
 
 server.listen(5500, "127.0.0.1", () =>
-  console.log("Server Running On Port 5500")
+	console.log("Server Running On Port 5500")
 );
